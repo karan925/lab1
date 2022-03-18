fc79c3df95f2f6348afbe7d0e5f847408cdbc05c
Karan Singh <ksingh209@gmail.com>
2022-03-18 13:36:04 -0700

    first commit

---
+++ api/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+
+app.use(cors())
+app.options('*', cors());
+
+app.get("/", function(req, res) {
+  res.send({"name": "Jane Doe"})
+})
+
+// var con = mysql.createConnection({
+//   user: "root",
+//   host: "localhost",
+//   password: "password",
+//   database: "login"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.query("CREATE DATABASE mydb", function (err, result) {
+//     if (err) throw err;
+//     console.log("Database created");
+//   });
+// });
+
+
+app.listen(3001, () => {
+  console.log("app listening on port 3001")
+})
